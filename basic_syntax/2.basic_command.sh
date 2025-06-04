e1d11f1761631b9458e510b3aca9e349c21f5459
YoungKwanK <young.kwin@gmail.com>
2025-06-04 10:31:10 +0900

    이력 정리

--- basic_syntax/2.basic_command.sh
+++ basic_syntax/2.basic_command.sh
@@ -10,3 +10,12 @@ git add testfolder/test1.txt
 git commit -m "메시지 제목" -m "메시지 내용"
 # git commit만 입력하고 ㅇ네티시 vi모드 -> 첫줄 : 타이틀, 두번째줄부터 내용 입력
 git commit
+
+# commit 이력 확인
+git log
+git log --oneline
+
+# 원격 저장소로 업로드
+git push origin 브랜치명
+# 충돌발생 시 충돌 무시하고, 로컬기준으로 원격에 덮어쓰기
+git push origin 브랜치명 --force
